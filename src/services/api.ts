d0f7a3270ff250a2bd99d4879299a106945763d4
hsgaiu <104129285+hsgaiu@users.noreply.github.com>
2024-12-25 17:00:24 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const API_URL = 'http://0.0.0.0:8000/api';
+const API_URL = 'https://0.0.0.0:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
