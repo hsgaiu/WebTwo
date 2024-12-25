ebdd15d419795e9fd397eb415e5f98fb9dfede65
hsgaiu <104129285+hsgaiu@users.noreply.github.com>
2024-12-25 19:08:33 +0300

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -77,7 +77,7 @@ systemctl enable memorial-backend
 systemctl start memorial-backend
 
 # Настройка SSL с помощью Certbot
-certbot --nginx -d granitmaster34.ru --non-interactive --agree-tos --email saitlesha@gmail.com
+certbot --nginx -d granitmaster34.ru --non-interactive --agree-tos --email rrp308531@gmail.com@gmail.com
 
 # Настройка автоматического обновления SSL-сертификата
 cat > /etc/cron.d/certbot << EOL
